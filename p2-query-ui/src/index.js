cd5dac81fcc3abe1914457e36227ad73b3bcf066
Franz Becker <franz.becker@itemis.de>
2017-06-20 14:31:58 +0200

    add font-awesome

--- p2-query-ui/src/index.js
+++ p2-query-ui/src/index.js
@@ -5,6 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
