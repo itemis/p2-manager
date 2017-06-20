bcc9b01ed3536d5ea0aa0d6fcc85b3a6a7e848d1
Franz Becker <franz.becker@itemis.de>
2017-06-20 11:01:54 +0200

    first attempts of loading data from the backend

--- p2-query-ui/src/TreeExample.js
+++ p2-query-ui/src/TreeExample.js
@@ -1,45 +1,54 @@
-'use strict';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Treebeard } from 'react-treebeard';
 
-const data = {
-  name: 'root',
-  toggled: true,
-  children: [
-    {
-      name: 'parent',
-      children: [
-        { name: 'child1' },
-        { name: 'child2' }
-      ]
-    },
-    {
-      name: 'loading parent',
-      loading: true,
-      children: []
-    },
-    {
-      name: 'parent',
-      children: [
-        {
-          name: 'nested parent',
-          children: [
-            { name: 'nested child 1' },
-            { name: 'nested child 2' }
-          ]
-        }
-      ]
-    }
-  ]
-};
+let data = [
+  {
+    "id": 1,
+    "name": "http://download.eclipse.org/oomph/updates/milestone",
+    "children": []
+  },
+  {
+    "id": 2,
+    "name": "http://download.eclipse.org/oomph/drops/milestone/S20170307-122318-1.7.0-M4",
+    "children": []
+  },
+  {
+    "id": 3,
+    "name": "http://download.eclipse.org/oomph/drops/milestone/S20170301-061518-1.7.0-M3",
+    "children": []
+  },
+  {
+    "id": 4,
+    "name": "http://download.eclipse.org/oomph/drops/milestone/S20170215-125145-1.7.0-M2",
+    "children": []
+  },
+  {
+    "id": 5,
+    "name": "http://download.eclipse.org/oomph/drops/milestone/S20170201-120440-1.7.0-M1",
+    "children": []
+  }
+]
 
 class TreeExample extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
     this.onToggle = this.onToggle.bind(this);
+    fetch(`http://localhost:8080/repositories`)
+      .then(response => response.json())
+      .then(json => {
+        let x = json.map(element => {
+          return {
+            name: element.uri,
+            id: element.id,
+            children: []
+          }
+        });
+        console.log(x);
+      }
+
+      )
   }
   onToggle(node, toggled) {
     if (this.state.cursor) { this.state.cursor.active = false; }
