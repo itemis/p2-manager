9c723168c72103f93aab480a7a18a77f147ae0c1
Florian Stolte <fstolte@itemis.de>
2017-11-16 16:09:25 +0100

    Added more tests.

--- p2-manager-backend/src/test/java/com/itemis/p2m/backend/tests/BackendMockTest.java
+++ p2-manager-backend/src/test/java/com/itemis/p2m/backend/tests/BackendMockTest.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -28,6 +29,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -57,27 +60,120 @@ public class BackendMockTest {
     public void init() {
     	mapper = new ObjectMapper();
     }
+    
+    private void setupResponse(String response) throws Exception {
+    	ObjectNode responseObject = mapper.readValue(response, ObjectNode.class);
+    	when(neoRestTemplate.postForObject(eq(neo4jUrl), isA(Map.class), eq(ObjectNode.class))).thenReturn(responseObject);
+	}
 
+    //-----------------------RepositoryController-----------------------------
+    
     @Test
-    public void returnEmptyArrayWhenNeoHasNoRepositories() throws Exception {
-    	ObjectNode response = mapper.readValue("{\"data\":[]}", ObjectNode.class);
-    	when(neoRestTemplate.postForObject(eq(neo4jUrl), isA(Map.class), eq(ObjectNode.class))).thenReturn(response);
+    public void returnEmptyArrayIfNeoHasNoRepositories() throws Exception {
+    	this.setupResponse("{\"data\":[]}");
 		
         this.mockMvc.perform(get("/repositories")).andDo(print()).andExpect(status().isOk()).andExpect(content().json("[]"));
     }
 
     @Test
-    public void returnRepositoriesWhenNeoHasRepositories() throws Exception {
-    	ObjectNode response = mapper.readValue("{\"data\":[[16, \"http://www.allTheRepositories.com/theBestRepository\"],[42, \"http://www.justOneRepository.org\"]]}", ObjectNode.class);
-    	when(neoRestTemplate.postForObject(eq(neo4jUrl), isA(Map.class), eq(ObjectNode.class))).thenReturn(response);
-		
+    public void returnRepositoriesIfNeoHasRepositories() throws Exception {
+    	this.setupResponse("{\"data\":[[16, \"http://www.allTheRepositories.com/theBestRepository\"],[42, \"http://www.justOneRepository.org\"]]}");
+    	
         this.mockMvc.perform(get("/repositories"))
         			.andDo(print())
         			.andExpect(status().isOk())
         			.andExpect(content().string(containsString("16")))
-        			.andExpect(content().string(containsString("16")))
         			.andExpect(content().string(containsString("http://www.allTheRepositories.com/theBestRepository")))
         			.andExpect(content().string(containsString("42")))
         			.andExpect(content().string(containsString("http://www.justOneRepository.org")));
+    }
+    
+    @Test
+    public void returnTheQueriedRepository() throws Exception {
+    	this.setupResponse("{\"data\":[[16, \"http://www.allTheRepositories.com/theBestRepository\"],[42, \"http://www.justOneRepository.org\"]]}");
+		
+        this.mockMvc.perform(get("/repositories/16"))
+        			.andDo(print())
+        			.andExpect(status().isOk())
+        			.andExpect(content().string(containsString("16")))
+        			.andExpect(content().string(containsString("http://www.allTheRepositories.com/theBestRepository")));
+    }
+    
+    @Test
+    public void returnOnlyTheQueriedRepository() throws Exception {
+    	this.setupResponse("{\"data\":[[16, \"http://www.allTheRepositories.com/theBestRepository\"],[42, \"http://www.justOneRepository.org\"]]}");
+		
+        String result = this.mockMvc.perform(get("/repositories/16"))
+        			.andDo(print())
+        			.andReturn().getResponse().getContentAsString();
+        
+        assert !result.contains("42");
+        assert !result.contains("http://www.justOneRepository.org");
+    }
+    
+    @Test
+    public void returnIUsIfRepositoryHasIUs() throws Exception {
+    	this.setupResponse("{\"data\":[[\"com.company.framework.unit\", \"1.2.3\"], [\"org.organization.tool\", \"9.0.1\"]]}");
+		
+        this.mockMvc.perform(get("/repositories/16/units"))
+        			.andDo(print())
+        			.andExpect(status().isOk())
+        			.andExpect(content().string(containsString("com.company.framework.unit")))
+        			.andExpect(content().string(containsString("1.2.3")))
+					.andExpect(content().string(containsString("org.organization.tool")))
+					.andExpect(content().string(containsString("9.0.1")));
+    }
+
+    //-----------------------InstallableUnitController-----------------------------
+
+    @Test
+    public void returnIUsIfNeoHasIUs() throws Exception {
+    	this.setupResponse("{\"data\":[[\"com.company.framework.unit\", \"1.2.3\"], [\"com.company.framework.unit\", \"1.5.2\"], [\"org.organization.tool\", \"9.0.1\"]]}");
+		
+        this.mockMvc.perform(get("/units"))
+        			.andDo(print())
+        			.andExpect(status().isOk())
+        			.andExpect(content().string(containsString("com.company.framework.unit")))
+        			.andExpect(content().string(containsString("1.2.3")))
+        			.andExpect(content().string(containsString("1.5.2")))
+					.andExpect(content().string(containsString("org.organization.tool")))
+					.andExpect(content().string(containsString("9.0.1")));
+    }
+    
+    @Test
+    public void returnAllVersionsOfTheQueriedIU() throws Exception {
+    	this.setupResponse("{\"data\":[[\"com.company.framework.unit\", \"1.2.3\"], [\"com.company.framework.unit\", \"1.5.2\"], [\"org.organization.tool\", \"9.0.1\"]]}");
+		
+        this.mockMvc.perform(get("/units/com.company.framework.unit/versions"))
+        			.andDo(print())
+        			.andExpect(status().isOk())
+        			.andExpect(content().string(containsString("com.company.framework.unit")))
+        			.andExpect(content().string(containsString("1.2.3")))
+					.andExpect(content().string(containsString("1.5.2")));
+    }
+    
+    @Test
+    public void returnOnlyVersionsOfTheQueriedIU() throws Exception {
+    	this.setupResponse("{\"data\":[[\"com.company.framework.unit\", \"1.2.3\"], [\"com.company.framework.unit\", \"1.5.2\"], [\"org.organization.tool\", \"9.0.1\"]]}");
+		
+    	String result = this.mockMvc.perform(get("/units/com.company.framework.unit"))
+        			.andDo(print())        
+        			.andReturn().getResponse().getContentAsString();
+        
+        assert !result.contains("org.organization.tool");
+        assert !result.contains("9.0.1");
+    }
+    
+    @Test
+    public void returnRepositoriesWhenQueryingForUIVersions() throws Exception {
+    	this.setupResponse("{\"data\":[[16, \"http://www.allTheRepositories.com/theBestRepository\"],[42, \"http://www.justOneRepository.org\"]]}");
+		
+        this.mockMvc.perform(get("/units/com.company.framework.unit/versions/1.0.0/repositories"))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("16")))
+		.andExpect(content().string(containsString("http://www.allTheRepositories.com/theBestRepository")))
+		.andExpect(content().string(containsString("42")))
+		.andExpect(content().string(containsString("http://www.justOneRepository.org")));
     }
 }
