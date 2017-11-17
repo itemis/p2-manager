be23f9396948913bddeeb74023c8c250dd6c0fe2
Florian Stolte <fstolte@itemis.de>
2017-11-17 14:56:43 +0100

    Added CORS headers to p2m-backend services.

--- p2-manager-backend/src/main/java/com/itemis/p2m/backend/Application.java
+++ p2-manager-backend/src/main/java/com/itemis/p2m/backend/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.support.BasicAuthorizationInterceptor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -17,6 +20,16 @@ public class Application {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 	
 	@Bean
 	public RestTemplate queryServiceRestTemplateBean() {
