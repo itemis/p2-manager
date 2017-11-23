3d79ee01c46e848895c055af9a81aab7b0519dd3
Florian Stolte <fstolte@itemis.de>
2017-11-23 14:36:31 +0100

    Added debouncing for unit search and tidied up Controllers.js

--- p2-manager-frontend/public/Controllers.js
+++ p2-manager-frontend/public/Controllers.js
@@ -1,53 +1,64 @@
-var ng = angular.module('com.itemis.p2-manager-frontend', ['angular.filter', 'infinite-scroll'])
+"use strict";
 
-ng.controller('P2MController', function($scope, $http, $timeout) {
+const ng = angular.module('com.itemis.p2-manager-frontend', ['angular.filter', 'infinite-scroll'])
+
+ng.controller('P2MController', function($scope, $http, $timeout, $q) {
 	
-	$scope.getRepositories = function() {
+	$scope.getRepositories = () => {
 		$http.get('http://localhost:8080/repositories').
-        then(function(response) {
+        then(response => {
             $scope.repositories = response.data;
         });
 	}
 	
 	//TODO allow input without "http://" to be automatically completed
-	$scope.addRepository = function() {
-		$http.post('http://localhost:8080/repositories?uri='+$scope.repositoryURL).
-        then(function(response) {
-        	$timeout(function() {
+	$scope.addRepository = () => {
+		$http.post("http://localhost:8080/repositories?uri="+$scope.repositoryURL).
+        then(response => {
+			
+        	$timeout(() => {
         		$scope.getRepositories();
         	}, 1000);
-        }); 
+		}); 
+	    
 	}
 	
-	$scope.searchUnits = function() {
+	$scope.searchUnits = () => {
+		if ($scope.searchTimeout !== undefined) {
+			$scope.searchTimeout.resolve();
+		}
+		$scope.searchTimeout = $q.defer();
+
 		$scope.units = [];
 		$scope.allUnitsLoaded = false;
 		$scope.loadMoreUnits();
 	}
 	
-	$scope.loadMoreUnits = function() {
-		if ($scope.unitsAreLoading || $scope.allUnitsLoaded)
+	$scope.loadMoreUnits = () => {
+		if ($scope.unitsAreLoading || $scope.allUnitsLoaded) {
 			return;
+		}
 		
 		$scope.unitsAreLoading = true;
-		var searchQuery = $scope.unitId.split(" ")
+		const searchQuery = $scope.unitId.split(" ")
 								.map(keyword => "searchTerm="+keyword.replace(/\s/g, ''))
 								.reduce((keyword1, keyword2) => keyword1+"&"+keyword2);
 
 		$http.get('http://localhost:8080/units?limit='+$scope.scrollLoadSize
 											+"&offset="+$scope.units.length
 											+"&"+searchQuery)
-		.then(function(response) {
+		.then(response => {
 			$scope.unitsAreLoading = false;
 			
-			if (response.status == 204) // No Content
+			if (response.status === 204) { // No Content 
 				$scope.allUnitsLoaded = true;
-			else
+			} else {
 				$scope.units = $scope.units.concat(response.data);
+			}
 		});
 	}
 	
-	$scope.isValidUnitId = function(unitId) {
+	$scope.isValidUnitId = (unitId) => {
 		return unitId.includes("/") || unitId.includes("\"");
 	}
 
@@ -65,10 +76,10 @@ ng.controller('P2MController', function($scope, $http, $timeout) {
 
 ng.controller('RepositoryUnitController', function($scope, $http) {
 	
-	$scope.getUnitsForRepo = function() {
+	$scope.getUnitsForRepo = () => {
 		if (!$scope.showUnits)
 			$http.get('http://localhost:8080/repositories/'+$scope.repository.repoId+'/units').
-				then(function(response) {
+				then(response => {
 					$scope.unitsInRepository = response.data;
 				});
 		
@@ -78,10 +89,10 @@ ng.controller('RepositoryUnitController', function($scope, $http) {
 
 ng.controller('UnitController', function($scope, $http, $timeout) {
 	
-	$scope.getRepositoriesForVersion = function() {
+	$scope.getRepositoriesForVersion = () => {
 		if (!$scope.repositoriesLoaded) {
 			$http.get('http://localhost:8080/units/'+$scope.unit.unitId+'/versions/'+$scope.unit.version+"/repositories").
-				then(function(response) {
+				then(response => {
 					$scope.repositoriesWithVersion = response.data;
 				});
 			$scope.repositoriesLoaded = true;
