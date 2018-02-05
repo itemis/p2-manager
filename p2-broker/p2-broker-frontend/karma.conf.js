51fe0e31cd265473fe9d4ff606b24bfd1b6013ec
Florian Stolte <florian.stolte@itemis.de>
2018-02-05 11:59:18 +0100

    Added testing support for frontend.
    - moved to bower for client-side dependencies
    - setup karma and jasmine for testing angular.js app

---
+++ p2-broker/p2-broker-frontend/karma.conf.js
@@ -0,0 +1,39 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: './public',
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-messages/angular-messages.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-material/angular-material.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+	  
+      'app/**/*.module.js',
+      'app/**/*.js'
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
+
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
