197dcf4b94de99bceaa56aa1ff923c75df9437a3
Florian Stolte <fstolte@itemis.de>
2018-09-21 10:13:09 +0200

    Add Unit tests for Neo4JQueryBuilder and improve its implementation.

--- p2-broker/p2-broker-backend/src/main/java/com/itemis/p2m/backend/Neo4JQueryBuilder.java
+++ p2-broker/p2-broker-backend/src/main/java/com/itemis/p2m/backend/Neo4JQueryBuilder.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.StringJoiner;
 
+import com.google.common.base.Preconditions;
+
 public class Neo4JQueryBuilder {
 	
 	class QueryParts {
@@ -20,30 +22,37 @@ public class Neo4JQueryBuilder {
 	private QueryParts parts = new QueryParts();
 
 	public Neo4JQueryBuilder filterContains(String property, String substring) {
+		Preconditions.checkNotNull(property);
+		Preconditions.checkNotNull(substring);
 		return this.filter(property+" CONTAINS '"+substring+"'");
 	}
 	
 	public Neo4JQueryBuilder filter(String condition) {
+		Preconditions.checkNotNull(condition);
 		parts.filters.add(condition);
 		return this;
 	}
 	
 	public Neo4JQueryBuilder match(String pattern) {
+		Preconditions.checkNotNull(pattern);
 		parts.matchPattern = pattern;
 		return this;
 	}
 	
 	public Neo4JQueryBuilder create(String pattern) {
+		Preconditions.checkNotNull(pattern);
 		parts.createPattern = pattern;
 		return this;
 	}
 	
 	public Neo4JQueryBuilder result(String pattern) {
+		Preconditions.checkNotNull(pattern);
 		parts.returnPattern = pattern;
 		return this;
 	}
 	
 	public Neo4JQueryBuilder orderBy(String property) {
+		Preconditions.checkNotNull(property);
 		parts.orderByProperty = property;
 		return this;
 	}
@@ -54,9 +63,25 @@ public class Neo4JQueryBuilder {
 	}
 	
 	public Neo4JQueryBuilder limit(String limit, String offset) {
-		parts.limit = Integer.parseInt(limit) <= 0 ? "" : " LIMIT "+limit;
-		parts.skipAmount = Integer.parseInt(offset) <= 0 ? "" : " SKIP "+offset;
+		int parsedLimit = parseLimitArgument(limit);
+		int parsedOffset = parseLimitArgument(offset);
+		parts.limit = parsedLimit == 0 ? "" : " LIMIT "+limit;
+		parts.skipAmount = parsedOffset == 0 ? "" : " SKIP "+offset;
 		return this;
+	}
+
+	private int parseLimitArgument(String input) {
+		Preconditions.checkNotNull(input);
+		int result;
+		try {
+			result = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(String.format("{} is not a valid integer number!", input));
+		}
+		if (result < 0) {
+			throw new IllegalArgumentException(String.format("{} is not a positive integer number!", input));
+		}
+		return result;
 	}
 
 	@Override
@@ -68,7 +93,7 @@ public class Neo4JQueryBuilder {
 	}
 	
 	public Map<String, String> buildMap() {
-		if (parts.matchPattern == null) {
+		if (parts.matchPattern.equals("")) {
 			throw new IllegalStateException("A match must be set via Neo4JQueryBuilder::match before the query can be built!");
 		}
 		
@@ -81,14 +106,14 @@ public class Neo4JQueryBuilder {
 	}
 	
 	private String matchClause() {
-		if (parts.matchPattern == null || parts.matchPattern.equals("")) {
+		if (parts.matchPattern.equals("")) {
 			return "";
 		}
 		return "MATCH "+parts.matchPattern;
 	}
 	
 	private String createClause() {
-		if (parts.createPattern == null || parts.createPattern.equals("")) {
+		if (parts.createPattern.equals("")) {
 			return "";
 		}
 		return " CREATE "+parts.createPattern;
@@ -105,7 +130,7 @@ public class Neo4JQueryBuilder {
 	}
 	
 	private String returnClause() {
-		if (parts.returnPattern == null || parts.returnPattern.equals("")) {
+		if (parts.returnPattern.equals("")) {
 			return "";
 		} else {
 			StringBuilder clause = new StringBuilder();
@@ -114,7 +139,7 @@ public class Neo4JQueryBuilder {
 				clause.append("DISTINCT ");
 			}
 			clause.append(parts.returnPattern);
-			if (parts.orderByProperty != null && !parts.orderByProperty.equals("")) {
+			if (!parts.orderByProperty.equals("")) {
 				clause.append(" ORDER BY ");
 				clause.append(parts.orderByProperty);
 			}
---
+++ p2-broker/p2-broker-backend/src/test/java/com/itemis/p2m/backend/Neo4JQueryBuilderTest.java
@@ -0,0 +1,91 @@
+package com.itemis.p2m.backend;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.itemis.p2m.backend.Neo4JQueryBuilder;
+
+public class Neo4JQueryBuilderTest {
+	
+	private Neo4JQueryBuilder query;
+	
+	@Before
+	public void init() {
+		query = new Neo4JQueryBuilder();
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void buildMapRequiredMatch() {
+		query.buildMap();
+	}
+	
+	@Test
+	public void builtMapContainsQueryString() {
+		query.match("(x)").result("x");
+		String expectation = query.toString();
+		assertEquals(expectation, query.buildMap().get("query"));
+	}
+	
+	@Test
+	public void buildBasicMatchQuery() {
+		String expectation = "MATCH (x) RETURN x";
+		query.match("(x)").result("x");
+		assertEquals(expectation, query.toString());
+	}
+	
+	@Test
+	public void buildQueryWithContainsFilter() {
+		String expectation = "MATCH (x) WHERE x CONTAINS 'someString' RETURN x";
+		query.match("(x)").result("x").filterContains("x", "someString");
+		assertEquals(expectation, query.toString());
+	}
+	
+	@Test
+	public void buildQueryWithTwoFiltersAndOrderByAndLimitAndDistinct() {
+		String expectation = "MATCH (x) WHERE x.y = 'a' AND x.z = 'b' RETURN DISTINCT x ORDER BY x.y SKIP 3 LIMIT 6";
+		query.match("(x)").filter("x.y = 'a'").filter("x.z = 'b'").result("x").orderBy("x.y").limit("6", "3").distinct();
+		assertEquals(expectation, query.toString());
+	}
+	
+	@Test
+	public void buildQueryWithEmptyArguments() {
+		String expectation = "";
+		query.match("").filter("").result("").orderBy("").limit("0", "0");
+		assertEquals(expectation, query.toString());
+	}
+	
+	@Test
+	public void queryWithoutResultHasNoOrderByAndLimitAndSkipAndDistinct() {
+		String expectation = "MATCH (x)";
+		query.match("(x)").orderBy("x.y").limit("6", "3").distinct();
+		assertEquals(expectation, query.toString());
+	}
+	
+	@Test
+	public void buildQueryWithFilterAndCreate() {
+		String expectation = "MATCH (x),(y) WHERE x.z = y.z CREATE (x)-[:ABC]->(y) RETURN x,y,z";
+		query.match("(x),(y)").filter("x.z = y.z").create("(x)-[:ABC]->(y)").result("x,y,z");
+		assertEquals(expectation, query.toString());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void passFalselyFormattedStringToLimitMethod() {
+		query.limit("a", "3");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void passNegativeNumberToLimitMethod() {
+		query.limit("3", "-3");
+	}
+	
+	@Test
+	public void resetClearsAllPreviousMethodCalls() {
+		String expectation = "";
+		query.match("(x)").filter("x.y = 'a'").filter("x.z = 'b'").result("x").orderBy("x.y").limit("6", "3").distinct().reset();
+		assertEquals(expectation, query.toString());
+	}
+}
